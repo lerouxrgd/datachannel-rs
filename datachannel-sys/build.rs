b830e08a30719f7326627c4674e02d076d1cd6b0
Romain Leroux <romain@leroux.dev>
2020-06-25 21:12:48 +0200

    Add static build feature

--- datachannel-sys/build.rs
+++ datachannel-sys/build.rs
@@ -4,21 +4,53 @@ use std::path::PathBuf;
 fn main() {
     let out_dir = env::var("OUT_DIR").unwrap();
 
-    // let mut config = cmake::Config::new("libdatachannel");
-    // config.build_target("datachannel-static");
-    // config.out_dir(&out_dir);
-    // config.define("USE_JUICE", "1");
-    // config.define("USE_GNUTLS", "1");
-    // config.build();
+    if cfg!(feature = "static") {
+        let mut config = cmake::Config::new("libdatachannel");
+        config.build_target("datachannel-static");
+        config.out_dir(&out_dir);
+        config.define("USE_JUICE", "1");
+        config.build();
+    }
 
     let mut config = cmake::Config::new("libdatachannel");
     config.out_dir(&out_dir);
     config.define("USE_JUICE", "1");
-    config.define("USE_GNUTLS", "1");
     config.build();
 
-    println!("cargo:rustc-link-search=native={}/lib", out_dir);
-    println!("cargo:rustc-link-lib=dylib=datachannel");
+    if cfg!(feature = "static") {
+        // Link static libc++
+        #[cfg(feature = "static")]
+        cpp_build::Config::new()
+            .include(format!("{}/lib", out_dir))
+            .build("src/lib.rs");
+
+        // Link static openssl
+        println!("cargo:rustc-link-search=native={}/lib", out_dir);
+        println!("cargo:rustc-link-lib=static=crypto");
+        println!("cargo:rustc-link-lib=static=ssl");
+
+        // Link static libjuice
+        println!(
+            "cargo:rustc-link-search=native={}/build/deps/libjuice",
+            out_dir
+        );
+        println!("cargo:rustc-link-lib=static=juice-static");
+
+        // Link static usrsctplib
+        println!(
+            "cargo:rustc-link-search=native={}/build/deps/usrsctp/usrsctplib",
+            out_dir
+        );
+        println!("cargo:rustc-link-lib=static=usrsctp");
+
+        // Link static libdatachannel
+        println!("cargo:rustc-link-search=native={}/build", out_dir);
+        println!("cargo:rustc-link-lib=static=datachannel-static");
+    } else {
+        // Link dynamic libdatachannel
+        println!("cargo:rustc-link-search=native={}/lib", out_dir);
+        println!("cargo:rustc-link-lib=dylib=datachannel");
+    }
 
     let bindings = bindgen::Builder::default()
         .header(format!("{}/include/rtc/rtc.h", out_dir))
